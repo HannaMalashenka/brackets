2d33c0a839f5469fe7831d9a49b20c59abbbda84
anika <hannamalashenka@gmail.com>
2020-10-25 23:38:26 +0300

    feat: solve brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = Object.fromEntries(bracketsConfig);
+  const notClosedBrackets = [];
+
+  for (e of str.split('')) {
+      if (notClosedBrackets[0] == e) {
+        notClosedBrackets.shift();
+      } else if (brackets[e]) {
+        notClosedBrackets.unshift(brackets[e]);
+      } else {
+          return false;
+      }
+  }
+  return notClosedBrackets.length == 0;  
 }
